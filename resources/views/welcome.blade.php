7ef1f424fe2067d7250b58e75e2ee0a0df3bcdc6
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-10-31 08:37:29 +0700

    feat: Project Setup

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>Sample Page</title>
+
+        <!-- !!! THE LINE BELOW IS REQUIRED SO YOU CAN USE BOOTSTRAP !!! -->
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+        <!-- !!! THE LINE ABOVE IS REQUIRED SO YOU CAN USE BOOTSTRAP !!! -->
+    </head>
+    <body>
+        Hello World!
+    </body>
+</html>
