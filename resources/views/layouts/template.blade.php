0a47fcc3a815c778766161b1306d792487c28616
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-12-08 12:46:51 +0700

    Try deploying again

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -4,6 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
     <title>Eventure</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
