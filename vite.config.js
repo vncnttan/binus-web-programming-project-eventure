3ab4609c2d91b92214b8c756f377e0755173f01c
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-12-08 12:31:35 +0700

    fix: Adding app.css to the input laravel

--- vite.config.js
+++ vite.config.js
@@ -4,17 +4,8 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/sass/app.scss', 'resources/js/app.js'],
+            input: ['resources/sass/app.scss', 'resources/js/app.js', "resources/css/app.css"],
             refresh: true,
         }),
-    ],
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        rollupOptions: {
-            input: {
-                'app': './resources/css/app.css',
-            },
-        },
-    }
+    ]
 });
