00f2f35626486129d0631db2c82fa9e7247616ff
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-12-08 12:15:30 +0700

    Trying without middleware

--- routes/web.php
+++ routes/web.php
@@ -9,19 +9,19 @@ use App\Http\Middleware\EnsureRoleValid;
 use App\Http\Middleware\GuestAuthenticate;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(GuestAuthenticate::class)->group(function () {
+//Route::middleware(GuestAuthenticate::class)->group(function () {
     Route::get('/login', [UserController::class, 'login'])->name('login');
     Route::get('/register', [UserController::class, 'register'])->name('register');
     Route::post('/register', [UserController::class, 'registerUser'])->name('register.user');
     Route::post('/login', [UserController::class, 'loginUser'])->name('login.user');
-});
+//});
 
-Route::middleware([Authenticate::class, EnsureRoleNotFilled::class])->group(function () {
+//Route::middleware([Authenticate::class, EnsureRoleNotFilled::class])->group(function () {
     Route::get('/choose-role', [UserController::class, 'chooseRole'])->name('choose-role');
     Route::post('/choose-role', [UserController::class, 'chooseRoleUser'])->name('choose-role.user');
-});
+//});
 
-Route::middleware([Authenticate::class, EnsureRoleValid::class])->group(function () {
+//Route::middleware([Authenticate::class, EnsureRoleValid::class])->group(function () {
     Route::get('/logout', [UserController::class, 'logout'])->name('logout');
     Route::get('/', [EventController::class, 'index'])->name('index');
     Route::get('/event/{event}', [EventController::class, 'show'])->name('event.show');
@@ -33,5 +33,5 @@ Route::middleware([Authenticate::class, EnsureRoleValid::class])->group(function
     Route::get('/events/add', [EventController::class, 'add'])->name('events.add');
     Route::put('/events/{event}', [EventController::class, 'update'])->name('events.update');
     Route::post('/events/store', [EventController::class, 'store'])->name('event.store');
-});
+//});
 
