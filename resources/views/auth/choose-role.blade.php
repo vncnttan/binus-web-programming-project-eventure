18602fbc7963d2d1f790c5ec15e95b3c227d8362
vncnttan <vincent.tanjaya.vt@gmail.com>
2024-12-14 14:58:12 +0700

    fix: Choose role local deployment

--- resources/views/auth/choose-role.blade.php
+++ resources/views/auth/choose-role.blade.php
@@ -1,13 +1,14 @@
 @extends("layouts.template")
 
+
 @section("content")
     <div class="w-100 text-center position-relative">
         <img class="img-fluid align-content-center w-100 position-fixed start-50 translate-middle-x"
-             src="{{storage_asset("/Login/choose-role-bg.png")}}"
+             src="{{ storage_asset("/Login/choose-role-bg.png") }}"
              alt="Loading Login Background" style="z-index: -10; bottom: -24%"/>
 
         <div class="container h-100">
-            <img src="{{storage_asset("/General/Black Logo.png")}}" alt="Eventure Logo" class="img-fluid d-block"
+            <img src="{{ storage_asset("/General/Black Logo.png") }}" alt="Eventure Logo" class="img-fluid d-block"
                  style="width: 200px; margin-top: 2rem;"/>
             <h1 class="fw-bold mt-5">
                 Welcome to Eventure!
@@ -19,20 +20,25 @@
                 </svg>
             </h1>
             <p style="margin-top: -0.7rem">We’re glad you’re here! What can we help you with?</p>
-            <form action="{{route("choose-role.user")}}" method="POST" class="mt-5 row text-center mx-auto w-75 gap-5 text-black" style="width: fit-content">
+            <form action="{{ route("choose-role.user") }}" method="POST"
+                  class="mt-5 row text-center mx-auto w-75 gap-5 text-black" style="width: fit-content">
                 @csrf
                 <div class="col p-5 bg-white rounded-3 p-4">
                     <div class="my-4">
-                        {!! file_get_contents(storage_asset("/Login/find-experience-asset.svg")) !!}
+                        <img src="{{ storage_asset("/Login/find-experience-asset.svg") }}" alt="Find an Experience"
+                             class="img-fluid"/>
                     </div>
                     <div class="mb-4 ">
-                        Find and Experience
+                        Find an Experience
                     </div>
-                    <button type="submit" name="role" value="participant" class="btn btn-outline-dark mb-4">View Events around you</button>
+                    <button type="submit" name="role" value="participant" class="btn btn-outline-dark mb-4">View Events
+                        around you
+                    </button>
                 </div>
                 <div class="col p-5 bg-white rounded-3 p-4">
                     <div class="my-4">
-                        {!! file_get_contents(storage_asset("/Login/organize-events-asset.svg")) !!}
+                         <img src="{{ storage_asset("/Login/organize-events-asset.svg") }}" alt="Find an Experience"
+                             class="img-fluid"/>
                     </div>
                     <div class="mb-4 ">
                         Organize Events
